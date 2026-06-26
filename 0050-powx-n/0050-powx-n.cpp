f5faa7fa2ac679817ffc7478d8f1d3ad71cda17f
Ukani Meet Manishbhai <meet.ukani34@gmail.com>
2026-06-26 16:17:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (69.98%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long nn = n; 
+        if (nn < 0) {
+            nn = -nn;
+            x=1/x;
+        }
+        double ans = 1.0;
+        while (nn > 0) {
+            if (nn % 2 == 1) { 
+                ans = ans * x;
+                nn = nn - 1;
+            } else { 
+                x = x * x;
+                nn = nn / 2;
+            }
+        }        
+        return ans;
+    }
+};
