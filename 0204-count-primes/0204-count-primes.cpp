52a9034da39a4086e03f0b669c8ab9919ae4b4d0
Ukani Meet Manishbhai <meet.ukani34@gmail.com>
2026-07-01 10:53:18 +0530

    Time: 136 ms (94.64%), Space: 29.7 MB (23.33%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;
-        int count = 0;
-        vector<bool> isPrime(n + 1, 1);
-        isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i < n; i++)
-            if(isPrime[i])
-                for (int j = i*2; j < n; j+= i)
-                    isPrime[j] = 0;
-
-        for (int i = 2; i < n; i++)
-            if (isPrime[i])
-                count++;
-        
-        return count;
+        int c=0;
+        if (n<=2) return 0;
+        int prime[n+1];
+        for(int i=2;i<n;i++) {
+            prime[i]=1;
+        }
+        for(int i=2;i*i<=n;i++){
+            if (prime[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if (prime[i]==1){
+                c++;
+            }
+        }
+        return c;
     }
 };
