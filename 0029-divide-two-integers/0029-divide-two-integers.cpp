6448d4ec828770b1faa6a59d598a2416b622a508
Ukani Meet Manishbhai <meet.ukani34@gmail.com>
2026-07-06 21:11:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (72.51%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int divide(long long dividend, long long divisor) {
-        long long ans = dividend / divisor;
-        
-        if (ans > INT_MAX) {
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+        long long ans = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs;
+            long long multiple = 1;
+
+            while ((temp << 1) <= dvd) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            ans += multiple;
         }
-        if (ans < INT_MIN) {
-            return INT_MIN;
-        }
-        
-        return ans;
+
+        if ((dividend < 0) ^ (divisor < 0))
+            ans = -ans;
+
+        return (int)ans;
     }
 };
