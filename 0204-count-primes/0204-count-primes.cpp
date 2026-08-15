2e143e40862dc8c0d718608455ea7991c8f00333
Ukani Meet Manishbhai <meet.ukani34@gmail.com>
2026-08-15 14:28:09 +0530

    Time: 243 ms (17.75%), Space: 12.5 MB (91.61%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     int countPrimes(int n) {
         int c=0;
-        if (n<=2) return 0;
-        int prime[n+1];
-        for(int i=2;i<n;i++) {
-            prime[i]=1;
-        }
-        for(int i=2;i*i<=n;i++){
-            if (prime[i]==1){
-                for(int j=i*i;j<=n;j+=i){
-                    prime[j]=0;
-                }
-            }
-        }
+        vector<bool> a(n+1,true);
         for(int i=2;i<n;i++){
-            if (prime[i]==1){
+            if(a[i]){
                 c++;
+                for(int j=i*2;j<n;j=j+i){
+                    a[j] = false;
+                }
             }
         }
         return c;
